c4c49dff7944e57f3fc8706f1f62d016a8be4f3c
Bruno Rocha <brunos.roch@gmail.com>
2016-02-17 11:19:30 +0100

    Initial Commit

---
+++ Smaato/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Smaato
+//
+//  Created by Bruno Rocha on 2/17/16.
+//  Copyright © 2016 Bruno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Smaato/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Smaato
+//
+//  Created by Bruno Rocha on 2/17/16.
+//  Copyright © 2016 Bruno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
